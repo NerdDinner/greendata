4e5b68cdbec93f0b1c9e72ba9dc16a0224ca07f0
Farid <your_email@example.com>
2026-05-07 14:28:11 +0500

    оформление

--- check_order.py
+++ check_order.py
@@ -1,3 +1,6 @@
+#Написать алгоритмы по пункту 3 
+
+
 from datetime import datetime, time
 
 # проверка суммы заказа
--- sql/queries.sql
+++ sql/queries.sql
@@ -1,24 +1,45 @@
---Поиск двух самых дешевых товаров, дающих в сумме >= 3000
---6.1.	За два разных товара из интернет-магазина общей стоимостью 3 000 руб. и дороже
-WITH CheaperPair AS (
-    SELECT 
-        p1.NAME as p1_name, p1.PRICE as p1_price,
-        p2.NAME as p2_name, p2.PRICE as p2_price
-    FROM Products p1
-    JOIN Products p2 ON p1.ID < p2.ID -- исключить дубликаты и пары "товар сам с собой"
-    WHERE (p1.PRICE + p2.PRICE) >= 3000
-    ORDER BY (p1.PRICE + p2.PRICE) ASC
-    LIMIT 1
+--6.1.	За два разных товара из интернет-магазина общей стоимостью 3 000 руб. и дороже, дают третий товар в подарок. 
+--Для анализа возможных затрат на данную маркетинговую акцию владельцы магазина хотят получить представление о том, 
+--какие два самых дешевых товара будут соответствовать условиям акции, а какой товар (самый дорогой) будет предоставлен бесплатно. 
+
+--поиск двух самых дешевых товаров, в сумме >= 3000
+with cheaper_pair as (
+    select 
+        p1.name as p1_name, p1.price as p1_price,
+        p2.name as p2_name, p2.price as p2_price
+    from products p1
+    join products p2 on p1.id < p2.id -- исключить дубликаты и пары "товар сам с собой"
+    where (p1.price+p2.price) >= 3000
+    order by (p1.price+p2.price) asc
+    limit 1
 ),
---Поиск самого дорогого товара (подарок)
-MostExpensive AS (
-    SELECT NAME, PRICE FROM Products
-    ORDER BY PRICE DESC
-    LIMIT 1
+--поиск самого дорогого товара (подарок)
+most_expensive as (
+    select 
+        name, 
+        price 
+    from products
+    order by price desc
+    limit 1
 )
---Результат
-SELECT p1_name AS NAME, p1_price AS PRICE FROM CheaperPair
-UNION ALL
-SELECT p2_name, p2_price FROM CheaperPair
-UNION ALL
-SELECT NAME, PRICE FROM MostExpensive;
+--результат (SQL-запрос, который вернет три товара с учетом критериев, представленных в п. 6)
+
+--первый товар из пары дешевых
+select 
+    p1_name as name, 
+    p1_price as price 
+from cheaper_pair
+
+union all
+--второй товар из пары дешевых
+select 
+    p2_name, 
+    p2_price 
+from cheaper_pair
+
+union all
+--самый дешевый подарок
+select 
+    name, 
+    price 
+from most_expensive;
